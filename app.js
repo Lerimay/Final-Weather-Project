8c2f8fb3293f47f4c7d44e6bbfb2e6cd34373670
Lerimay <140885304+Lerimay@users.noreply.github.com>
2023-08-16 19:03:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,46 @@ function formatDate(timestamp) {
     ];
     let day = date.getDay();
     return `${day} ${hours}:${minutes}`;
+
+    function diplayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML =
+                forecastHTML +
+                `
+            <div class="col-2">
+              <div class="weather-forecast-date">
+                ${formatDay(
+                    forecastDay.dt
+                )} </div><img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon
+                }@2x.png" alt="" width="42px">
+               <div class="weather-forecast-temperatures">
+                 <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                )}°</span>
+                 <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                )}°</span>
+              </div>
+            </div>
+          `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+    let apiKey = "f52c42d33e007f7250df167ee104c7d8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(diplayForecast);
+}
 }
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
